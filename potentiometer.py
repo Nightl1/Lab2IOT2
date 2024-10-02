7f2f79b207831de15b6d68c2c688276e1333b384
Night <nightcorerock123@gmail.com>
2024-10-02 16:10:34 -0400

    The correct lab 2 code

--- potentiometer.py
+++ potentiometer.py
@@ -2,51 +2,43 @@
 import ADC0832
 import time
 import RPi.GPIO as GPIO
-
-# Set up the LED pin and PWM frequency
-LED_PIN = 4  # Pin connected to LED (alarm LED)
-PWM_FREQ = 1000  # 1kHz frequency for PWM
-
-def setup():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(LED_PIN, GPIO.OUT)  # Set up alarm LED pin
-
-    # Set up PWM for the LED
-    global pwm_led
-    pwm_led = GPIO.PWM(LED_PIN, PWM_FREQ)
-    pwm_led.start(0)  # Start with 0% duty cycle (LED off)
-
+ 
+LED = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED, GPIO.OUT)
+ 
+myPWM = GPIO.PWM(LED,10)
+myPWM.start(50)
+ 
+def init():
     ADC0832.setup()
-
+ 
 def loop():
     while True:
-        # Read ADC value from potentiometer
-        res = ADC0832.getADC(0)  # ADC channel 0
-        vol = 3.3 / 255 * res  # Convert to voltage
-        print('Digital value: %03d  ||  Voltage: %.2fV' % (res, vol))
-
-        # Scale ADC value (0-255) to PWM duty cycle (0-100%)
-        duty_cycle = res * 100 / 255
-        pwm_led.ChangeDutyCycle(duty_cycle)  # Adjust LED brightness
-
-        # Check luminosity level and control the alarm LED
-        if res < 10:  # Assuming the ADC value correlates directly to Lux
+        res = ADC0832.getADC(0)
+        vol = 3.3/255 * res
+        print ('analog value: %03d  ||  voltage: %.2fV' %(res, vol))
+        lux = res*100/255
+        myPWM.ChangeDutyCycle(lux)
+        time.sleep(0.2)
+ 
+        if res < 128:
             print("dark")
-            GPIO.output(LED_PIN, GPIO.LOW)  # Turn off alarm LED
+            GPIO.output(LED, GPIO.LOW)
         else:
             print("light")
-            GPIO.output(LED_PIN, GPIO.HIGH)  # Turn on alarm LED
-
-        time.sleep(0.2)  # Small delay for smoother updates
-
-def destroy():
-    pwm_led.stop()  # Stop PWM
-    GPIO.cleanup()  # Clean up GPIO
-
+            GPIO.output(LED, GPIO.HIGH)
+            
+       
+        lux = res * 100 / 255
+        myPWM.ChangeDutyCycle(lux)
+        print(lux)
+        time.sleep(0.2)
+ 
 if __name__ == '__main__':
+    init()
     try:
-        setup()
         loop()
     except KeyboardInterrupt:
-        destroy()
-        print('The end!')
+        ADC0832.destroy()
+        print ('The end !')
